8ad63b29bdf03925e231774ff2a4f0ba02378018
Ghisso <ghisso@gmail.com>
2019-07-05 16:38:44 +0900

    added command line parsing

--- Models/ImageLabeling.cs
+++ Models/ImageLabeling.cs
@@ -2,21 +2,11 @@ namespace ImageLabelingAvalonia.Models
 {
     public static class ImageLabeling
     {
-        public static readonly string input_path;
-        public static readonly string output_path;
-        public static readonly string labeling_name;
-        public static readonly string csv_name;
-        public static readonly string[] classes;
-        public static readonly bool isResuming;
-
-        static ImageLabeling()
-        {
-            input_path = @"/Users/ben.ghassen/Documents/Workstuff/Pole/inputfiles";
-            output_path = @"/Users/ben.ghassen/Documents/Workstuff/Pole";
-            labeling_name = "inputfiles-labeling";
-            csv_name = "results.csv";
-            classes = new string [] { "OK", "UNSURE", "NOTOK"};
-            isResuming = false;
-        }
+        public static string input_path;
+        public static string output_path;
+        public static string labeling_name;
+        public static string csv_name = "results.csv";
+        public static string[] classes;
+        public static bool isResuming;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using System.IO;
 using Avalonia;
 using Avalonia.Logging.Serilog;
 using ImageLabelingAvalonia.ViewModels;
 using ImageLabelingAvalonia.Views;
+using ImageLabelingAvalonia.Models;
 
 namespace ImageLabelingAvalonia
 {
@@ -11,7 +12,54 @@ namespace ImageLabelingAvalonia
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp().Start(AppMain, args);
+        public static void Main(DirectoryInfo input, DirectoryInfo output=null, string name = "null", string[] classes = null, bool resume = false)
+        {
+            if(!input.Exists)
+            {
+                System.Console.WriteLine("The input directory does not exists. Please enter an existing directory.");
+                return;
+            }
+            if(resume)
+            {
+
+            }
+            else
+            {
+                if(output != null  && !output.Exists)
+                {
+                    System.Console.WriteLine("The out directory does not exists. Please enter an existing directory.");
+                    return;
+                }
+                if(output == null)
+                    output = input.Parent;
+
+
+                if(name == null)
+                    name = input.Name + "-labeling";
+
+
+                if(classes != null && (classes.Length < 2 || classes.Length > 9))
+                {
+                    System.Console.WriteLine("The number of classes must be between 2 and 9.");
+                    return;
+                }
+                if(classes == null)
+                    classes = new[] {"OK", "UNSURE", "NOTOK"};
+
+                ImageLabeling.input_path = input.FullName;
+                ImageLabeling.output_path = output.FullName;
+                ImageLabeling.labeling_name = name;
+                ImageLabeling.classes = classes;
+                ImageLabeling.isResuming = resume;
+
+                // ImageLabeling.input_path = @"/Users/ben.ghassen/Documents/Workstuff/Pole/inputfiles";
+                // ImageLabeling.output_path = @"/Users/ben.ghassen/Documents/Workstuff/Pole/";
+                // ImageLabeling.labeling_name = "inputfiles-labels";
+                // ImageLabeling.classes = new[] {"abc", "def", "ghi", "jkl"};
+                // ImageLabeling.isResuming = resume;
+            }
+            BuildAvaloniaApp().Start(AppMain, null);
+        }  
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
